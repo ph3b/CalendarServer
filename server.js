88511cb3a329f1c447dd2dfbb90389e4f51c5bbe
Mathias <mathiasiden@me.com>
2015-03-02 19:33:07 +0100

    travis

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ module.exports = function(mode){
         }
         http.listen(port, ip);
         console.log('Server running on: ' + ip + ':' + port);
-        console.log(process.env.ENV_VARIABLE);
+        console.log(process);
     });
 };
