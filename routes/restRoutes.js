ce5fa657fe24b8d48e9b1763f11ceca23b3e42d4
Mathias <mathiasiden@me.com>
2015-03-13 11:51:26 +0100

    Refactor

--- routes/restRoutes.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by mattiden on 02.03.15.
- */
-var loginHandler = require('./handler/loginHandler.js');
-
-module.exports = function(app){
-    app.post('/login', loginHandler); // Logs in user and provides JWT
-};
