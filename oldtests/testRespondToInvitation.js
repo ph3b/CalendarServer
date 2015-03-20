8c8b565b830ef4b72d44841d431a9cbe5eae15ab
Mathias <mathiasiden@me.com>
2015-03-20 11:40:37 +0100

    Removed old tests

--- oldtests/testRespondToInvitation.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mattiden on 13.03.15.
- */
-var expect = require('expect.js');
-var io = require('socket.io-client');
-
-var apiUrl = "http://localhost:3000";
-
-var client = {"username" : "mathias", "user_id" : 1};
